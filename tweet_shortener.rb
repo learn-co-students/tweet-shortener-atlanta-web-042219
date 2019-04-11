8d237178296f438f9b3d015b8bb1091d4c4e8185
han xu <hanxu27@gmail.com>
2019-04-11 19:48:41 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,15 +1,15 @@
 # Write your code here.
 def dictionary
   dictionary = {
-    hello: "hi",
-    to: "2",
-    two: "2",
-    too: "2",
-    :for => "4",
-    four: "4",
-    be: "b",
-    at: "@",
-    :and => "&"
+    "hello"=> "hi",
+    "to"=> "2",
+    "two"=> "2",
+    "too"=> "2",
+    "for"=> "4",
+    "four"=> "4",
+    "be"=> "b",
+    "at"=> "@",
+    "and" => "&"
   }
 end
 
